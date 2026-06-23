9c367c922189e17b3537883db15648741436de59
Felipe Simoes <felipe_lima_simoes@hotmail.com>
2026-06-23 14:31:48 -0300

    change base on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Set to '/hexcrawler/' when deploying to GitHub Pages project site
-  base: '/',
+  base: process.env.VITE_BASE ?? '/',
 })
